1ad27a19adbbece3e5fdf8cd802f96431d23c0c2
Kristina <kristina.3435@gmail.com>
2020-11-02 19:10:19 +0100

    theme

--- CHIMP/main.cpp
+++ CHIMP/main.cpp
@@ -1,10 +1,19 @@
 #include "mainwindow.h"
-
+#include <unistd.h>
 #include <QApplication>
-
+#include <QFile>
+#include <limits.h>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    //App style:
+    QFile File(":/stylesheets/qss/MaterialDark.qss");
+    File.open(QFile::ReadOnly);
+    QString StyleSheet = QLatin1String(File.readAll());
+    qApp->setStyleSheet(StyleSheet);
+
+
     MainWindow w;
     w.show();
     return a.exec();
